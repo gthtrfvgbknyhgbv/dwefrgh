daad71f85bd9cfbbd63ef4a561d99b4bf452f0e3
gthtrfvgbknyhgbv <101848362+gthtrfvgbknyhgbv@users.noreply.github.com>
2022-03-17 21:36:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('read',  () => {
+    console.log('Code is running successful')
+})
+
+
+
+client.login('OTU0MTg4MDg3MDM3MTYxNDky.YjPe5w.ewpUOqLy6YEQ55OzIuU2nFgIRjg')
